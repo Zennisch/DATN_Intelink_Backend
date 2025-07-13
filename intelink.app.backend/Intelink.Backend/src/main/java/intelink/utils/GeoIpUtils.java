310b7096927a57276b97e7f6f570bc2e89b46250
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-07-13 12:01:29 +0900

    update: rename GeoIP resource directory and add tests
    Renamed the 'geo_lite_2' resource directory to 'geoLite2' and updated references in GeoIpUtils.java. Added GeoIpUtilsTests.java to provide basic unit testing for GeoIpUtils functionality.

--- intelink.app.backend/Intelink.Backend/src/main/java/intelink/utils/GeoIpUtils.java
+++ intelink.app.backend/Intelink.Backend/src/main/java/intelink/utils/GeoIpUtils.java
@@ -13,7 +13,7 @@ public class GeoIpUtils {
 
     static {
         try {
-            File database = new File("src/main/resources/geo_lite_2/GeoLite2-City.mmdb");
+            File database = new File("src/main/resources/geoLite2/GeoLite2-City.mmdb");
             databaseReader = new DatabaseReader.Builder(database).build();
         } catch (IOException e) {
             throw new RuntimeException("Failed to initialize GeoIP database reader", e);
