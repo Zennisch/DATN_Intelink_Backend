56845fa653950d509563fba0d1895dd4dd955ee4
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-07-19 04:11:36 +0900

    create: official backend project

---
+++ intelink.app.backend/intelink-project/src/test/java/intelink/intelinkproject/IntelinkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package intelink.intelinkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelinkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
