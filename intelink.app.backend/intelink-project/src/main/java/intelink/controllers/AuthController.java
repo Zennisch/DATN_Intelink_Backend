cd5168284f4162590877c945ccac383712812b37
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-07-29 23:48:37 +0900

    update: refactor

--- intelink.app.backend/intelink-project/src/main/java/intelink/controllers/AuthController.java
+++ intelink.app.backend/intelink-project/src/main/java/intelink/controllers/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
--- intelink.app.backend/intelink-project/src/main/java/intelink/dto/HourlyClick.java
+++ intelink.app.backend/intelink-project/src/main/java/intelink/dto/HourlyClick.java
@@ -6,8 +6,6 @@ import lombok.Data;
 @Data
 @Builder
 public class HourlyClick {
-
     private Integer hour;
     private Long totalClicks;
-
 }
