2350c4096a3d1edb7550183c4b7965146e69af92
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-08-05 13:05:11 +0900

    update

--- intelink.app.backend/intelink-project/src/main/java/intelink/services/interfaces/IAnalysisResultService.java
+++ intelink.app.backend/intelink-project/src/main/java/intelink/services/interfaces/IAnalysisResultService.java
@@ -1,7 +1,13 @@
 package intelink.services.interfaces;
 
+import intelink.models.AnalysisResult;
+
+import java.util.List;
+
 public interface IAnalysisResultService {
 
+    AnalysisResult save(AnalysisResult result);
 
+    List<AnalysisResult> saveAll(List<AnalysisResult> results);
 
 }
