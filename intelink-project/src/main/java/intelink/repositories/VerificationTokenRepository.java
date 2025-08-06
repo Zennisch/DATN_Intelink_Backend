3ba766dc544ed28596751a94e5e21e0b58123270
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-08-06 19:19:06 +0900

    create: repositories and data seeder utility
    Introduces OAuthAccountRepository and VerificationTokenRepository for JPA access to OAuthAccount and VerificationToken entities. Adds DataSeeder utility to generate sample data for users, short URLs, verification tokens, OAuth accounts, analysis results, click logs, and statistics for development and testing.

---
+++ intelink-project/src/main/java/intelink/repositories/VerificationTokenRepository.java
@@ -0,0 +1,7 @@
+package intelink.repositories;
+
+import intelink.models.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}
