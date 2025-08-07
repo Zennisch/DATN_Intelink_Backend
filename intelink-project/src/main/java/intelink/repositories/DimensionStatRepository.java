b935a721e8cd139084429f469e64db75970a5a44
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-08-07 14:49:58 +0700

    add: reposotories for analytic

--- intelink-project/src/main/java/intelink/repositories/DimensionStatRepository.java
+++ intelink-project/src/main/java/intelink/repositories/DimensionStatRepository.java
@@ -4,11 +4,19 @@ import intelink.models.DimensionStat;
 import intelink.models.ShortUrl;
 import intelink.models.enums.DimensionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DimensionStatRepository extends JpaRepository<DimensionStat, Long> {
     Optional<DimensionStat> findByShortUrlAndTypeAndValue(ShortUrl shortUrl, DimensionType type, String value);
+    
+    List<DimensionStat> findByShortUrlAndType(ShortUrl shortUrl, DimensionType type);
+    
+    @Query("SELECT ds FROM DimensionStat ds WHERE ds.shortUrl = :shortUrl AND ds.type = :type ORDER BY ds.totalClicks DESC")
+    List<DimensionStat> findByShortUrlAndTypeOrderByTotalClicksDesc(@Param("shortUrl") ShortUrl shortUrl, @Param("type") DimensionType type);
 }
