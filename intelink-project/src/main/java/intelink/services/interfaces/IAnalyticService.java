e340b842d574c28cdbf96204066a5fa233bcbdb4
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-08-07 14:46:40 +0700

    add: interface anlytic

--- intelink-project/src/main/java/intelink/services/interfaces/IAnalyticService.java
+++ intelink-project/src/main/java/intelink/services/interfaces/IAnalyticService.java
@@ -1,6 +1,10 @@
 package intelink.services.interfaces;
 
 import intelink.dto.helper.DimensionInfo;
+import intelink.models.enums.DimensionType;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public interface IAnalyticService {
 
@@ -8,4 +12,11 @@ public interface IAnalyticService {
 
     void recordClickStats(String shortCode);
 
+    ArrayList<Map<String, Object>> getDeviceStats(String shortCode);
+
+    ArrayList<Map<String, Object>> getLocationStats(String shortCode);
+
+    ArrayList<Map<String, Object>> getTimeStats(String shortCode);
+
+    ArrayList<Map<String, Object>> getDimensionStats(String shortCode, DimensionType type);
 }
