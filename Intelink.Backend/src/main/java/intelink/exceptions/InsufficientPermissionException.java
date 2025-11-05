2f15b954e53f7fd5dd66126851c65ca7e538093f
Zennisch <zennisch@gmail.com>
2025-11-05 15:04:20 +0900

    delete: old project

--- Intelink.Backend/src/main/java/intelink/exceptions/InsufficientPermissionException.java
+++
@@ -1,9 +0,0 @@
-package intelink.exceptions;
-
-import org.springframework.security.access.AccessDeniedException;
-
-public class InsufficientPermissionException extends AccessDeniedException {
-    public InsufficientPermissionException(String message) {
-        super(message);
-    }
-}
