4e4808159341522793a523a2eb341801bc96e719
Zennisch <zennisch@gmail.com>
2025-11-05 18:10:45 +0900

    create: new project

---
+++ intelink-server/src/main/java/intelink/intelink/server/IntelinkServerApplication.java
@@ -0,0 +1,13 @@
+package intelink.intelink.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelinkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelinkServerApplication.class, args);
+    }
+
+}
---
+++ intelink-server/src/test/java/intelink/intelink/server/IntelinkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package intelink.intelink.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelinkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
