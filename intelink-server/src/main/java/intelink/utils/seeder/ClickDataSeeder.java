be47c01080caf9539744c92ec0f541abc59fc7df
Zennisch <zennisch@gmail.com>
2025-12-13 18:09:09 +0900

    update

--- intelink-server/src/main/java/intelink/utils/seeder/ClickDataSeeder.java
+++ intelink-server/src/main/java/intelink/utils/seeder/ClickDataSeeder.java
@@ -61,6 +61,7 @@ public class ClickDataSeeder {
             String browser = utils.getRandomElement(utils.browsers);
             String os = utils.getRandomElement(utils.operatingSystems);
             String deviceType = utils.getRandomElement(utils.deviceTypes);
+            ClickStatus status = utils.getRandom().nextDouble() < 0.9 ? ClickStatus.ALLOWED : ClickStatus.BLOCKED;
 
             ClickLog clickLog = ClickLog.builder()
                     .ipVersion(IPVersion.IPV4)
@@ -69,7 +70,7 @@ public class ClickDataSeeder {
                     .referrer(utils.getRandom().nextDouble() < 0.6 ? "https://" + utils.getRandomElement(utils.domains) : null)
                     .timestamp(timestamp)
                     .shortUrl(randomShortUrl)
-                    .status(ClickStatus.ALLOWED)
+                    .status(status)
                     .build();
 
             clickLogs.add(clickLog);
@@ -91,19 +92,24 @@ public class ClickDataSeeder {
                                 .bucketEnd(bucketEnd)
                                 .totalClicks(0L)
                                 .allowedClicks(0L)
+                                .blockedClicks(0L)
                                 .shortUrl(randomShortUrl)
                                 .build());
                 
                 ClickStat stat = clickStatsMap.get(statsKey);
                 stat.setTotalClicks(stat.getTotalClicks() + 1);
-                stat.setAllowedClicks(stat.getAllowedClicks() + 1);
+                if (status == ClickStatus.ALLOWED) {
+                    stat.setAllowedClicks(stat.getAllowedClicks() + 1);
+                } else {
+                    stat.setBlockedClicks(stat.getBlockedClicks() + 1);
+                }
             }
 
-            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.COUNTRY, country);
-            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.CITY, city);
-            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.BROWSER, browser);
-            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.OS, os);
-            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.DEVICE_TYPE, deviceType);
+            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.COUNTRY, country, status);
+            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.CITY, city, status);
+            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.BROWSER, browser, status);
+            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.OS, os, status);
+            createDimensionStat(dimensionStatsMap, randomShortUrl, DimensionType.DEVICE_TYPE, deviceType, status);
 
             if ((i + 1) % batchSize == 0 || i == clickLogCount - 1) {
                 clickLogRepository.saveAll(clickLogs);
@@ -130,18 +136,23 @@ public class ClickDataSeeder {
         // }
     }
 
-    private void createDimensionStat(Map<String, DimensionStat> dimensionStatsMap, ShortUrl shortUrl, DimensionType type, String value) {
+    private void createDimensionStat(Map<String, DimensionStat> dimensionStatsMap, ShortUrl shortUrl, DimensionType type, String value, ClickStatus status) {
         String key = shortUrl.getId() + "_" + type + "_" + value;
         dimensionStatsMap.computeIfAbsent(key, k -> DimensionStat.builder()
                         .type(type)
                         .value(value)
                         .totalClicks(0L)
                         .allowedClicks(0L)
+                        .blockedClicks(0L)
                         .shortUrl(shortUrl)
                         .build());
         
         DimensionStat stat = dimensionStatsMap.get(key);
         stat.setTotalClicks(stat.getTotalClicks() + 1);
-        stat.setAllowedClicks(stat.getAllowedClicks() + 1);
+        if (status == ClickStatus.ALLOWED) {
+            stat.setAllowedClicks(stat.getAllowedClicks() + 1);
+        } else {
+            stat.setBlockedClicks(stat.getBlockedClicks() + 1);
+        }
     }
 }
